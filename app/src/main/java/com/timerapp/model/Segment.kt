ef217d22463addd4659c0a4f00475931c41d9313
Matt Lang <M.Lang@Live.com>
2025-11-05 19:39:24 -0700

    Add trigger and actions to Segment

--- app/src/main/java/com/timerapp/model/Segment.kt
+++ app/src/main/java/com/timerapp/model/Segment.kt
@@ -1,5 +1,9 @@
 package com.timerapp.model
 
+import com.timerapp.model.Trigger.Trigger
+
 data class Segment(
-    val name: String
+    val name: String,
+    val actions: Array<Action<*>> = emptyArray(),
+    val trigger: Trigger
 )
