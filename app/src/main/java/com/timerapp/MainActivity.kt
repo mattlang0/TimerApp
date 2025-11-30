a766844303ce82b5e86d5a17229400db77851789
Matt Lang <M.Lang@Live.com>
2025-11-30 16:44:03 -0700

    Remove unused Settings menu

--- app/src/main/java/com/timerapp/MainActivity.kt
+++ app/src/main/java/com/timerapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.timerapp
 
 import android.os.Bundle
-import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
@@ -53,12 +52,6 @@ class MainActivity : AppCompatActivity() {
                 binding.appBarMain.fab.hide()
             }
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
